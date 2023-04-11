7aec11797673f5e4a4b397bbc97267e0b55e6d69
JoseBravo1996 <bravo.jose.luis18@gmail.com>
2023-04-11 19:38:37 -0300

    change api pagination

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,14 @@
 const API_URL = 'https://api.giphy.com/v1/gifs/trending?api_key=Bt1uIy2S9LonG1XUiGliN7XGs1OgLmcX&limit=25&rating=g';
 const MAIN = document.getElementById('menu-container');
-const GENRES = ['accion', 'aventura', 'crimen', 'comedia', 'drama'];
-getData();
 
-function showMovies(data) {
+
+getData();
+// let btnNext;
+// let btnPrevious;
+// let templateHtml;
+
+
+function showGifs(data) {
     MAIN.innerHTML = '';
     data.data.forEach(movie => {
         const box = document.createElement('div');
@@ -13,25 +18,20 @@ function showMovies(data) {
             <div class="box-img">
             <img src="${movie.images.original.url}">
         </div>
-        <h2>${movie.title}</h2>
-        <h3>${movie.source}</h3>
-        <span>$10</span>
+        <!--  <h2>${movie.title}</h2> -->
+        <!-- <h3>${movie.source}</h3> -->
+        <!-- <span>$10</span> -->
         <i class="bx bx-heart"></i>
         </div>`
 
         MAIN.appendChild(box);
     });
+
+    // btnNext = data.pagination.offset ? `<button>Next</button>` : '';
+    // btnPrevious = data.pagination.offset ? `<button>Previous</button>` : '';
+
 }
 
-function getColor(vote) {
-    if (vote >= 4) {
-        return 'green';
-    } else if (vote >= 2) {
-        return 'orange';
-    } else {
-        return 'red';
-    }
-}
 
 function getData() {
     let url = `${API_URL}`;
@@ -44,11 +44,23 @@ function getData() {
 
         if (this.status == 200 && this.readyState == 4) {
             let datos = JSON.parse(this.responseText);
-            showMovies(datos);
+            showGifs(datos);
             afterLoad();
         }
     }
 }
+
+
+function getColor(vote) {
+    if (vote >= 4) {
+        return 'green';
+    } else if (vote >= 2) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
 
 function createElement(genre, movie) {
     const section = document.getElementById(genre);
